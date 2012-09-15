17a39a4b5efa3ed294da2710361f98d4086ad863
Wiliam Alexandre da Costa <wiliam_costa@yahoo.com.br>
2012-09-14 22:32:56 -0300

    Criacao Visual Inicial

---
+++ CineC/CineC/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace CineC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelGenero = new System.Windows.Forms.Label();
+            this.labelLocal = new System.Windows.Forms.Label();
+            this.labelData = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.comboBoxGen = new System.Windows.Forms.ComboBox();
+            this.textBoxLocal = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderNome = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderGen = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderLocal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderData = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonAdicionar = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonRemover = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(12, 56);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(35, 13);
+            this.labelNome.TabIndex = 0;
+            this.labelNome.Text = "Nome";
+            // 
+            // labelGenero
+            // 
+            this.labelGenero.AutoSize = true;
+            this.labelGenero.Location = new System.Drawing.Point(524, 56);
+            this.labelGenero.Name = "labelGenero";
+            this.labelGenero.Size = new System.Drawing.Size(42, 13);
+            this.labelGenero.TabIndex = 1;
+            this.labelGenero.Text = "Gênero";
+            // 
+            // labelLocal
+            // 
+            this.labelLocal.AutoSize = true;
+            this.labelLocal.Location = new System.Drawing.Point(12, 107);
+            this.labelLocal.Name = "labelLocal";
+            this.labelLocal.Size = new System.Drawing.Size(112, 13);
+            this.labelLocal.TabIndex = 2;
+            this.labelLocal.Text = "Local que foi Assistido";
+            // 
+            // labelData
+            // 
+            this.labelData.AutoSize = true;
+            this.labelData.Location = new System.Drawing.Point(524, 107);
+            this.labelData.Name = "labelData";
+            this.labelData.Size = new System.Drawing.Size(30, 13);
+            this.labelData.TabIndex = 3;
+            this.labelData.Text = "Data";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(15, 72);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(492, 20);
+            this.textBoxNome.TabIndex = 4;
+            // 
+            // comboBoxGen
+            // 
+            this.comboBoxGen.FormattingEnabled = true;
+            this.comboBoxGen.Items.AddRange(new object[] {
+            "Ação",
+            "Aventura",
+            "Comédia",
+            "Terror",
+            "Suspense",
+            "Documentário",
+            "Infantil",
+            "Romance",
+            "Ficção Científica"});
+            this.comboBoxGen.Location = new System.Drawing.Point(527, 72);
+            this.comboBoxGen.Name = "comboBoxGen";
+            this.comboBoxGen.Size = new System.Drawing.Size(235, 21);
+            this.comboBoxGen.TabIndex = 5;
+            // 
+            // textBoxLocal
+            // 
+            this.textBoxLocal.Location = new System.Drawing.Point(12, 123);
+            this.textBoxLocal.Name = "textBoxLocal";
+            this.textBoxLocal.Size = new System.Drawing.Size(495, 20);
+            this.textBoxLocal.TabIndex = 6;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(527, 123);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(235, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderNome,
+            this.columnHeaderGen,
+            this.columnHeaderLocal,
+            this.columnHeaderData});
+            this.listView1.Location = new System.Drawing.Point(15, 173);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(747, 247);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderNome
+            // 
+            this.columnHeaderNome.Text = "Nome";
+            this.columnHeaderNome.Width = 114;
+            // 
+            // columnHeaderGen
+            // 
+            this.columnHeaderGen.Text = "Gênero";
+            this.columnHeaderGen.Width = 135;
+            // 
+            // columnHeaderLocal
+            // 
+            this.columnHeaderLocal.Text = "Local que foi Assistido";
+            this.columnHeaderLocal.Width = 249;
+            // 
+            // columnHeaderData
+            // 
+            this.columnHeaderData.Text = "Data";
+            this.columnHeaderData.Width = 186;
+            // 
+            // buttonAdicionar
+            // 
+            this.buttonAdicionar.Location = new System.Drawing.Point(522, 426);
+            this.buttonAdicionar.Name = "buttonAdicionar";
+            this.buttonAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdicionar.TabIndex = 9;
+            this.buttonAdicionar.Text = "Adicionar";
+            this.buttonAdicionar.UseVisualStyleBackColor = true;
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(604, 426);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEditar.TabIndex = 10;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemover
+            // 
+            this.buttonRemover.Location = new System.Drawing.Point(686, 426);
+            this.buttonRemover.Name = "buttonRemover";
+            this.buttonRemover.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemover.TabIndex = 11;
+            this.buttonRemover.Text = "Remover";
+            this.buttonRemover.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 466);
+            this.Controls.Add(this.buttonRemover);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.buttonAdicionar);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBoxLocal);
+            this.Controls.Add(this.comboBoxGen);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.labelData);
+            this.Controls.Add(this.labelLocal);
+            this.Controls.Add(this.labelGenero);
+            this.Controls.Add(this.labelNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.Label labelGenero;
+        private System.Windows.Forms.Label labelLocal;
+        private System.Windows.Forms.Label labelData;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.ComboBox comboBoxGen;
+        private System.Windows.Forms.TextBox textBoxLocal;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderNome;
+        private System.Windows.Forms.ColumnHeader columnHeaderGen;
+        private System.Windows.Forms.ColumnHeader columnHeaderLocal;
+        private System.Windows.Forms.ColumnHeader columnHeaderData;
+        private System.Windows.Forms.Button buttonAdicionar;
+        private System.Windows.Forms.Button buttonEditar;
+        private System.Windows.Forms.Button buttonRemover;
+    }
+}
+
